92436f42041b0cc030bce90855dd917d7a7a6cfa
Hashini Prabuddhika <131321861+HashiniPrabuddhika@users.noreply.github.com>
2024-06-09 21:43:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
   build: {
